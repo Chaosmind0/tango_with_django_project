57e813e020dfaea5418a54cdf6c70a301eecdf82
Qikai Zhou <2616452q@student.gla.ac.uk>
2024-02-05 15:48:43 +0000

    Comment

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,12 +1,44 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+# Import the Category model
+from rango.models import Category
+
+from rango.models import Page
 
 def index(request):
+    category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('views')[:5]
+
     context_dict = {
-        'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'
-    }
+                    'categories': category_list, 
+                    'pages': page_list,
+                    'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'
+                }
+    # Render the response and send it back!
     return render(request, 'rango/index.html', context=context_dict)
 # Create your views here.
 
 def about(request):
     return render(request, 'rango/about.html')
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    # Go render the response and return it to the client.
+    return render(request, 'rango/category.html', context_dict)
+
+
+
+
+
+
+
