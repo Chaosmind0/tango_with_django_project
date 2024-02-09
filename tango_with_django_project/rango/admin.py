bb87a9e48e8370c33dcefa72e6e10551e1ffeb8a
Qikai Zhou <2616452q@student.gla.ac.uk>
2024-02-09 09:37:23 +0000

    commit

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from rango.models import Category, Page, PageAdmin
+from rango.models import UserProfile
 
 
 admin.site.register(Page, PageAdmin)
 # Register your models here.
 
+admin.site.register(UserProfile)
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name',)}
